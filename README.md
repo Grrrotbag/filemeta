f34cd2fc2de2a1c5a42067b198346cdfeff7dd60
Gerard Keane <gerard.keane@dfkeane.com>
2021-01-19 22:01:28 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Filemeta
+
+A simple microservice to display the metadata from a supplied file.
+
+Uses:
+
+- [multer](https://www.npmjs.com/package/multer)
---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+
+// =============================================================================
+// Config
+// =============================================================================
+const app = express();
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(cors());
+app.use("/public", express.static(process.cwd() + "/public"));
+
+// =============================================================================
+// Server
+// =============================================================================
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.post("/upload", upload.single("upfile"), (req, res) => {
+  console.log("{name: ", req.file.originalname, "type: ", req.file.mimetype, "size:", req.file.size, "}");
+  return !req.file
+    ? res.send("No file attached.")
+    : res.json({ name: req.file.originalname, type: req.file.mimetype, size: req.file.size });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("Your app is listening on port " + port);
+});
